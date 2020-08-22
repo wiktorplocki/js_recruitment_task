bd7d1ed2b53fac719ec3e001033bdb052c09b1f8
Wiktor Płocki <wiktor.plocki@gmail.com>
2020-08-22 11:51:58 +0200

    Write API client, fetch data on load

--- src/main.js
+++ src/main.js
@@ -1,3 +1,36 @@
 import './styles/main.css';
 
-// Please use https://open-platform.theguardian.com/documentation/
+const GUARDIAN_API_KEY = '5beb9e2f-11b4-4b39-8b48-67be4429c7e6';
+const GUARDIAN_API_URL = 'https://content.guardianapis.com';
+
+const apiClient = async (path, params) => {
+  function getQueryStringFromObject(object) {
+    return Object.keys(object)
+      .map((key) => key + '=' + object[key])
+      .join('&');
+  }
+
+  if (params) {
+    path += '?' + getQueryStringFromObject(params);
+  }
+
+  const response = await fetch(`${GUARDIAN_API_URL}/${path}`, {
+    method: 'GET', // Hard-coded GET for this task only
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const result = await response.json();
+  return result;
+};
+
+let apiResult; // for storing API calls
+
+const sectionSelect = document.querySelector('#sectionSelect');
+
+(async () => {
+  apiResult = await apiClient('/search', { 'api-key': GUARDIAN_API_KEY });
+  console.log(apiResult);
+  console.log(sectionSelect.value);
+})();
